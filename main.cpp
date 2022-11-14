8bb0a05e224b7ebcd18b241d5cb7eaa84e89485e
Moriatsushi <k021g1463@g.neec.ac.jp>
2022-11-14 11:48:58 +0900

    コールバック変更

--- main.cpp
+++ main.cpp
@@ -5,55 +5,45 @@
 
 typedef void (*PFunc)(int *);
 //コールバック関数
-void DispResult(int* s) {
-    printf("%d秒待って実行されたよ\n", *s);
+void DispResult(int* n) {
+    //ランダム
+    srand(time(nullptr));
+
+    printf("%d秒待って実行されたよ\n", *n);
+
+    int getRand = rand();
+
+    getRand = getRand % 2;
+
+    if (getRand % 2 == *n) {
+        printf("アタリ\n");
+    }
+    else
+    {
+        printf("ハズレ\n");
+    }
 }
 
-void setTimerout(PFunc p, int second) {
+void setTimerout(PFunc p, int second ,int number) {
     //コールバック関数を呼び出す
     Sleep(second * 1000);
 
-    p(&second);
+    p(&number);
 }
 
 int main() {
-    //ランダム
-    srand(time(nullptr));
-   
-    int getRand = rand();
 
     int player = 0;
-    //数字の入力
-    scanf_s("%d", &player);
 
-    getRand = getRand % 2;
+    int number = 0;
+    //数字の入力
+    printf("半なら奇数、丁なら偶数を入力してください\n");
+    scanf_s("%d", &player);
 
     PFunc p;
 
     p = DispResult;
 
-    setTimerout(p, 3);
-
-    if (player == 0) {
-        if (getRand == 0) {
-
-            printf("あたり");
-        }
-        else {
-
-            printf("はずれ");
-        }
-    }
-    else if (player == 1) {
-        if (getRand == 1) {
-
-            printf("あたり");
-        }
-        else {
-
-            printf("はずれ");
-        }
-    }
-
+    setTimerout(p, 3 , number);
     return 0;
 }
