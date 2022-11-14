fa67efb71f956828f1536447086da2dd4a43db9e
Moriatsushi <k021g1463@g.neec.ac.jp>
2022-11-14 10:44:14 +0900

    関数ポインタとコールバック関数

--- main.cpp
+++ main.cpp
@@ -1,26 +1,59 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
 
-int RecursiveSalary(int n) {
-	const int general = 100;
-	if (n == 1) {
-
-		return general;
-		
-	}
-	return (RecursiveSalary(n - 1) * 2 - 50);
+typedef void (*PFunc)(int *);
+//コールバック関数
+void DispResult(int* s) {
+    printf("%d秒待って実行されたよ\n", *s);
 }
- 
+
+void setTimerout(PFunc p, int second) {
+    //コールバック関数を呼び出す
+    Sleep(second * 1000);
+
+    p(&second);
+}
+
 int main() {
+    //ランダム
+    srand(time(nullptr));
+   
+    int getRand = rand();
 
-	int generalSalary = 0;
-	int recursiveSalary = 0;
-	for (int i = 1; i < 9; i++) {
-		generalSalary += 1072;
-		recursiveSalary += RecursiveSalary(i);
+    int player = 0;
+    //数字の入力
+    scanf_s("%d", &player);
 
-		printf("一般的な賃金 : %d\n ", generalSalary);
-		printf("再帰的な賃金 : %d\n ", recursiveSalary);
-	}
+    getRand = getRand % 2;
 
-	return 0;
+    PFunc p;
+
+    p = DispResult;
+
+    setTimerout(p, 3);
+
+    if (player == 0) {
+        if (getRand == 0) {
+
+            printf("あたり");
+        }
+        else {
+
+            printf("はずれ");
+        }
+    }
+    else if (player == 1) {
+        if (getRand == 1) {
+
+            printf("あたり");
+        }
+        else {
+
+            printf("はずれ");
+        }
+    }
+
+    return 0;
 }
