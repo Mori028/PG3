eb0f91b1f56302f8db03a7573728febba0fc1e46
Moriatsushi <k021g1463@g.neec.ac.jp>
2023-01-30 03:19:53 +0900

    シングルトンパターン完成

--- main.cpp
+++ main.cpp
@@ -1,81 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <iostream>
-#include <list>
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+#include"SceneManager.h"
 
-int main(void) {
+int main() {
+	//インスタンスを取得してすぐ使用
+	SceneManager* sceneManager = SceneManager::GetInstance();
 
-	// 山手線駅名 
-	std::list<const char*> yamanoteLine = {
-		"Tokyo",
-		"Kanda",
-		"Akihabara",
-		"Okachimachi",
-		"Ueno",
-		"Uguisudani",
-		"Nippori",
-		"Tabara",
-		"Komagome",
-		"Sugamo",
-		"Otsuka",
-		"Ikebukuro",
-		"Mejiro",
-		"Takadanobaba",
-		"Shin-Okubo",
-		"Shinjuku",
-		"Yoyogi",
-		"Harajuku",
-		"Shibuya",
-		"Ebisu",
-		"Meguro",
-		"Gotanda",
-		"Osaki",
-		"Shinagawa",
-		"Tamachi",
-		"Hamamatsutyo",
-		"Shinbashi",
-		"Yurakutyo"
-	};
+	int change = 0;
 
+	while (true) {
 
-	// 山手線1970年の駅名
-	printf("-----1970年-----\n");
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
-		std::cout << *itr << "\n";
-	}
+		sceneManager->ChangeScene(change);
 
-	// 西日暮里駅を挿入
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
-		if (*itr == "Tabara") {
-			itr = yamanoteLine.insert(itr, "Nishi-Nippori");
-			++itr;
+		if (change >= 4) {
+
+			change = 0;
 		}
-	}
+		else {
 
-	// 山手線2019年の駅名
-	printf("\n-----2019年-----\n");
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
-		std::cout << *itr << "\n";
-	}
-
-	// 高輪ゲートウェイ駅を挿入
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
-		if (*itr == "Tamachi") {
-			itr = yamanoteLine.insert(itr, "Takanawa Gateway");
-			++itr;
+			change++;
 		}
+
+		Sleep(1 * 1000);
+
 	}
 
-	// 山手線2022年の駅名
-	printf("\n-----2022年-----\n");
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
-
-		std::cout << *itr << "\n";
-	}
-
-	system("Pause");
-
+	system("pause");
 	return 0;
 }
-
